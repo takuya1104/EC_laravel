2108f9386f162215e8573eba7a6d59c26998436f
TakuyaMurayama <takuya.msb@gmail.com>
2019-05-30 03:09:26 +0900

    add stylesheet追加

--- resources/views/item/detail.blade.php
+++ resources/views/item/detail.blade.php
@@ -1,22 +1,33 @@
 <!DOCTYPE html>
 <html lang ="ja">
 <html>
+<style>
+table{
+  border-collapse:collapse;
+  margin:0 auto;
+}
+
+td,th{
+  border-top:1px solid #666;
+  padding:10px;
+}
+</style>
 <body>
+<table>
 <th>商品名</th>
 <th>商品説明</th>
 <th>価格</th>
 <th>在庫</th>
-<table>
-		<tr>
-			<td>{{ $item->item_name }}</a></td>
-			<td>{{ $item->description }}</td>
-			<td>{{ $item->price }}</td>
-			@if ($item->stock > 0)
-			<td>在庫有り</td>
-			@else
-			<td>在庫なし</td>
-			@endif
-		</tr>
+<tr>
+<td>{{ $item->item_name }}</a></td>
+<td>{{ $item->description }}</td>
+<td>{{ $item->price }}</td>
+@if ($item->stock > 0)
+<td>在庫有り</td>
+@else
+<td>在庫なし</td>
+@endif
+</tr>
 </table>
 </body>
 </html>
