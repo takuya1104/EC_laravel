4a07763ec91b8753a4dc4fc650adcbeb33cb3454
TakuyaMurayama <takuya.msb@gmail.com>
2019-05-30 12:02:56 +0900

    fix useを使ってパスが短く記述できるようにした

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Item;
 class ItemController extends Controller
 {
 	public function index() {
-		$items = \App\Item::all();
+		$items = Item::all();
 		return view('item/index', compact('items'));
 	}
 	public function detail($id) {
-		$item = \App\Item::find($id);
+		$item = Item::find($id);
 		return view('item/detail', compact('item'));
 	}
 }
