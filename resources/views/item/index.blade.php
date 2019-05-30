50b615bd66f07dee048f865aa96c7a4c9635331c
TakuyaMurayama <takuya.msb@gmail.com>
2019-05-30 15:34:08 +0900

    fix tableタグ位置

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -7,18 +7,17 @@
 <th>商品説明</th>
 <th>価格</th>
 <th>在庫</th>
-{{-- コメント--}}
 @foreach ($items as $item)
-		<tr>
-			<td><a href ="{{ route('item.detail', ['id' => $item->id]) }}">{{ $item->item_name }}</a></td>
-			<td>{{ $item->description }}</td>
-			<td>{{ $item->price }}</td>
-			@if ($item->stock > 0)
-			<td>在庫有り</td>
-			@else
-			<td>在庫なし</td>
-			@endif
-		</tr>
+<tr>
+<td><a href ="{{ route('item.detail', ['id' => $item->id]) }}">{{ $item->item_name }}</a></td>
+<td>{{ $item->description }}</td>
+<td>{{ $item->price }}</td>
+@if ($item->stock > 0)
+<td>在庫有り</td>
+@else
+<td>在庫なし</td>
+@endif
+</tr>
 @endforeach
 </table>
 </body>
