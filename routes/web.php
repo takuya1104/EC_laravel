084fc0e1e5001f2d75ce30af2d99a9e4fd5ef3fd
TakuyaMurayama <takuya.msb@gmail.com>
2019-06-06 18:37:58 +0900

    ??

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 
 Auth::routes();
@@ -18,4 +18,17 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'ItemController@index');
 
-Route::get('/item/{id}', 'ItemController@detail')->name('item.detail');
+Route::get('/item/detail/{id}', 'ItemController@detail')->name('item.detail');
+
+//adminログイン不要
+Route::group(['prefix' => 'admin'], function() {
+	Route::get('/', function () { return redirect('/admin/home'); });
+	Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+	Route::post('login', 'Admin\LoginController@login');
+});
+
+//adminログイン後
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
+	Route::post('logout', 'Admin\LoginController@logout')->name('admin.logout');
+	Route::get('home', 'Admin\HomeController@index')->name('admin.home');
+});
