42e003ef85588aaec5af94939608fd5a4e235691
TakuyaMurayama <takuya.msb@gmail.com>
2019-06-09 09:47:47 +0900

    add リダイレクト先の指定

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,26 +1,25 @@
 <?php
-
 namespace App\Http\Middleware;
-
 use Closure;
 use Illuminate\Support\Facades\Auth;
-
 class RedirectIfAuthenticated
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
-    public function handle($request, Closure $next, $guard = null)
-    {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
-        }
-
-        return $next($request);
-    }
+	public function handle($request, Closure $next, $guard = null)
+	{
+		$redir = '/home';
+		switch($guard){
+			//ログイン時はログイン画面ではなく、/admin/hmeに
+		case "admin":
+			$redir = '/admin/home';
+			break;
+		default:
+			$redir = '/home';
+			break;
+		}
+		if (Auth::guard($guard)->check()) {
+			return redirect($redir);
+		}
+		return $next($request);
+	}
 }
+
