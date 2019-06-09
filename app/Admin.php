32398c1c19fdee4dcd406bb5e6d788d42a80ad7c
TakuyaMurayama <takuya.msb@gmail.com>
2019-06-09 09:51:09 +0900

    new 管理者モデル作成

--- app/Admin.php
+++ app/Admin.php
@@ -10,20 +10,10 @@ class Admin extends Authenticatable
 	use Notifiable;
 	protected $guard = 'admin';
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
 	protected $fillable = [
 		'name', 'email', 'password',
 	];
 
-	/**
-	 * The attributes that should be hidden for arrays.
-	 *
-	 * @var array
-	 */
 	protected $hidden = [
 		'password', 'remember_token',
 	];
