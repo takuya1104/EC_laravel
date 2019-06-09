eb65b8fd4b4763772d915103bd9669707186d8b6
TakuyaMurayama <takuya.msb@gmail.com>
2019-06-09 09:38:51 +0900

    new 管理者コントローラー作成

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -9,52 +9,32 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-		/*
-				|--------------------------------------------------------------------------
-					| Login Controller
-						|--------------------------------------------------------------------------
-							|
-								| This controller handles authenticating users for the application and
-									| redirecting them to your home screen. The controller uses a trait
-										| to conveniently provide its functionality to your applications.
-											|
-		 */
 
 	use AuthenticatesUsers;
 
-	/**
-	 * Where to redirect users after login.
-	 *
-	 * @var string
-	 */
 	protected $redirectTo = '/admin/home';
-
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
 	public function __construct()
 	{
 		$this->middleware('guest:admin')->except('logout');
 	}
 	public function showLoginForm()
 	{
-		return view('admin.login');  //変更
+		return view('admin.login');
 	}
 
 	protected function guard()
 	{
-		return Auth::guard('admin');  //変更
+		return Auth::guard('admin');
 	}
 
 	public function logout(Request $request)
 	{
-		Auth::guard('admin')->logout();  //変更
+		Auth::guard('admin')->logout();
 		$request->session()->flush();
+
 		$request->session()->regenerate();
 
-		return redirect('/admin/login');  //変更
+		return redirect('/admin/login');
 	}
 }
 
