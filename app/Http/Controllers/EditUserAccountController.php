56501d4d061710939036cfb5477cbdc6131f506c
TakuyaMurayama <takuya.msb@gmail.com>
2019-12-02 18:10:13 +0900

    メール送信機能作成

--- app/Http/Controllers/EditUserAccountController.php
+++ app/Http/Controllers/EditUserAccountController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Mail\Mailer;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EditEmailAddress;
 use App\Http\Requests\CreateAddress;
 use App\Providers\Validator;
 use App\User;
@@ -36,21 +39,36 @@ class EditUserAccountController extends Controller
 
 		//新規パスワードが8文字以上の文字列であり、確認用新規パスワードが新規パスワードと同じである事を確認
 		//現在登録済みユーザーが８文字以下の可能性があるのでusing_passwordには文字数のバリデーションはかけない
+		//パスワードは変更しない場合ははNULL可能その場合はメールアドレスの変更が条件分岐になって、名前とメールアップデート
+		//パスワードは変更する場合はの場合はメールアドレスの変更が条件分岐になって、名前とメールアップデートとパスワード
+
 		$request->validate([
-			'new_password' => 'required|min:8|string',
-			'confirm_password' => 'required|min:8|string',
-			'using_password' => 'required|string',
+			'new_password' => 'min:8|string|nullable',
+			'confirm_password' => 'min:8|string|nullable|same:new_password',
+			'using_password' => 'required|string|',
 		]);
 
 		//ハッシュ化されたパスと入力されたパスが同じか照合
 		if(!(Hash::check($request->using_password, Auth::user()->password))) {
-			session()->flash('flash_message', '現在のパスワードが間違っています');
+			session()->flash('flash_message', '入力したパスワードと現在のパスワードが間違っています');
 			return redirect(url()->previous());
 		}
 
-		//ログインユーザーのアドレスと入力されたアドレスが同じか称号
+		//ユーザー情報の変更がない場合、変更した文言のみ出力
+		$password = Hash::make($request->new_password);
+		//ログインユーザーのアドレスと入力されたアドレスが同じか照合
 		if (Auth::user()->email !== $request->user_email) {
+			$mail_to = "takuya2.msbal.";
+			$url = url('/editemailaddress');
+			Mail::to($mail_to)->send(new EditEmailAddress($url));
+			session()->flash('flash_message', '確認用メールを送信しました');
+			return redirect(url()->previous());
 		} else {
+			//ログインユーザーのアドレスと入力されたアドレスが同じ場合
+			if ($request->new_password === NULL && $request->confirm_password === NULL) {
+			}
+			session()->flash('flash_message', 'パスワードを変更しました');
+			return redirect(url()->previous());
 		}
 
 		return view('edit_user_account.receive_input');
