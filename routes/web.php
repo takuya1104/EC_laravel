dc1096599d189accd324289a92cabb6030bf89bd
TakuyaMurayama <takuya.msb@gmail.com>
2019-12-08 16:05:38 +0900

    未ログイン状態の場合の対応

--- routes/web.php
+++ routes/web.php
@@ -36,11 +36,10 @@ Route::group(['prefix' => '/', 'middleware' => 'auth'], function() {
 	Route::delete('cart', 'CartController@delete')->name('cart.delete');
 	//ユーザー情報編集
 	Route::get('edit_user_account/{id}', 'EditUserAccountController@index')->name('edit_user_account');
-
 	Route::post('edit_user_account/receive_input', 'EditUserAccountController@receive_input')->name('edit_user_account.receive_input');
+	Route::get('edit_user_account/receive_email/{token}', 'EditUserAccountController@receive_email')->name('edit_user_account.receive_email');
 });
 
-Route::get('edit_user_account/receive_email/{token}', 'EditUserAccountController@receive_email')->name('edit_user_account.receive_email');
 //住所追加編集削除
 
 Route::group(['prefix' => '/address', 'middleware' => 'auth'], function() {
